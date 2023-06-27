9ee94e1f064648f921704d4f5f4319b4fed21e16
Cat Pien <catarina.pien@gmail.com>
2023-06-27 16:25:09 -0700

    Updated plot of flow values

--- make_month_subsetgroup_plots.R
+++ make_month_subsetgroup_plots.R
@@ -36,31 +36,53 @@ Flow.subset <- flow.months %>%
                       Sac.cfs >= 0.75*Sac.quant[[3]], "E", "")
   )
 
+Flow_vals <- flow.months %>%
+  mutate(A = Sac.cfs )
 
+# Make flow values table for Suzanne ---------------------------------------------------
+Flowvals = data.frame(Group = c("A", "B", "C", "D", "E"),
+                      Description = c("Med SAC Low SJR", "Med SAC Med SJR",
+                                      "Med SAC High SJR", "Low SAC Med SJR",
+                                      "High SAC Med SJR"),
+                      minSAC = c(0.75*Sac.quant[[2]],0.75*Sac.quant[[2]],
+                                 0.75*Sac.quant[[2]],0.75*Sac.quant[[1]],
+                                 0.75*Sac.quant[[3]]),
+                      maxSAC = c(1.25*Sac.quant[[2]],1.25*Sac.quant[[2]],
+                                1.25*Sac.quant[[2]],1.25*Sac.quant[[1]],
+                                1.25*Sac.quant[[3]]),
+                      minSJR = c(0.75*Sjr.quant[[1]],0.75*Sjr.quant[[2]],
+                                 0.75*Sjr.quant[[3]],0.75*Sjr.quant[[2]],
+                                 0.75*Sjr.quant[[2]]),
+                      maxSJR = c(1.25*Sjr.quant[[1]],1.25*Sjr.quant[[2]],
+                                 1.25*Sjr.quant[[3]], 1.25*Sjr.quant[[2]],
+                                 1.25*Sjr.quant[[2]])) %>%
+  mutate(across(c(minSAC:maxSJR), ~round(., 1))) %>%
+  mutate("Sac Flow Range (cfs)" = paste0(minSAC, "-", maxSAC),
+         "SJR Flow Range (cfs)" = paste0(minSJR, "-", maxSJR)) %>%
+  dplyr::select(Group, Description, `Sac Flow Range (cfs)`, `SJR Flow Range (cfs)`)
 
+# write_csv(Flowvals, "data_export/flow_subset_values.csv")
 
-ggplot2::ggplot(flow_table)+ geom_point(aes(Sac.cfs, SJR.cfs, color = factor(Month)), size = 3) +
-  theme_bw()
+# Remake the median plot
+(plot_allvals <- ggplot() +
+  geom_vline(xintercept = Sac.quant[[2]], linetype = "dashed")+
+  geom_hline(yintercept = Sjr.quant[[2]], linetype = "dashed")+
+  geom_point(data = flow.months, aes(x=Sac.cfs, y=SJR.cfs), shape = 15, size = 2, color = "gray80") +
+  geom_point(data = flow_table %>%filter(OMR>-6500), aes(x = Sac.cfs, y = SJR.cfs, color = Sub.group, shape = factor(OMR)), size = 4) +
+    scale_shape_manual(values = c(21, 22, 23)) +
+  scale_color_viridis_d(option = "turbo")+
+  labs(x = "Sac Flow (cfs)", y = "SJR Flow (cfs)", color = "Inflow Group", shape = "OMR Flow (cfs)") +
+  theme_bw() +
+    theme(axis.text = element_text(size = 12),
+          axis.title = element_text(size = 11),
+          legend.text = element_text(size = 11),
+          legend.title = element_text(size = 12),
+          legend.position = "top"))
 
-A <- filter(Flow.subset, A == "A")
-ggplot2::ggplot(A)+ geom_point(aes(Sac.cfs, SJR.cfs, color = factor(Month)), size = 3) +
-  theme_bw()
+png("figures/allflowvals_groups_plot.png", width = 8, height = 5, units = "in", res = 300, pointsize = 12)
+plot_allvals
+dev.off()
 
-B <- filter(Flow.subset, B == "B")
-ggplot2::ggplot(B)+ geom_point(aes(Sac.cfs, SJR.cfs, color = factor(Month)), size = 3) +
-  theme_bw()
-
-C <- filter(Flow.subset, C == "C")
-ggplot2::ggplot(C)+ geom_point(aes(Sac.cfs, SJR.cfs, color = factor(Month)), size = 3) +
-  theme_bw()
-
-D <- filter(Flow.subset, D == "D")
-ggplot2::ggplot(D)+ geom_point(aes(Sac.cfs, SJR.cfs, color = factor(Month)), size = 3) +
-  theme_bw()
-
-E <- filter(Flow.subset, E == "E")
-ggplot2::ggplot(E)+ geom_point(aes(Sac.cfs, SJR.cfs, color = factor(Month)), size = 3) +
-  theme_bw()
 
 
 # These are plots I sent to Suzanne ---------------------------------------
