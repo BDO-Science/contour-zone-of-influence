e7cc8595797e002f7a68c123ab295c93bf369e96
Cat Pien <catarina.pien@gmail.com>
2023-06-27 17:13:38 -0700

    Added maps of ZOI

--- channel_lengths_updatedDSM2.R
+++ channel_lengths_updatedDSM2.R
@@ -2,7 +2,7 @@
 #### Catarina Pien and Lisa Elliott
 
 # This code uses zone of influence modeling results (DSM2) to create barplots showing
-#  how channel length associated with the zone of influence changes from operational facilities based on pumping 
+#  how channel length associated with the zone of influence changes from operational facilities based on pumping
 #to different OMR levels, at different SJR and SR flow levels
 
 # Packages
@@ -34,7 +34,7 @@ channels <- read_csv("data_updatedDSM2/Reclamation_2021LTO_DSM2_Version806_Chann
 
 
 # Drop nodes that are causing issues
-dropNodes <- c(146, 147, 148, 206, 242, 246)
+dropNodes <- c(146, 147, 148, 206, 242, 246, 432, 433, 434)
 nodes <- channels[!channels$upnode %in% dropNodes, ]
 nodes <- nodes[!nodes$downnode %in% dropNodes, ]
 channels <- nodes
@@ -50,7 +50,8 @@ zoi_channel <- merge(zoi_data, channels, by = "channel_number")
 zoi_channel_group <- zoi_channel %>%
   pivot_longer(cols = c(A, B, C, D, E),
               names_to = "Group",
-               values_to = "p_overlap")
+               values_to = "p_overlap") %>%
+  filter(p_overlap>=0)
 
 filtered_dat_high <- zoi_channel_group %>%
   filter(p_overlap < 0.25)
@@ -76,7 +77,7 @@ filtered2_med <- filtered_dat_med %>%
   mutate(pLength = sumLength/total_channel_length) %>%
   mutate(h_influence = "Medium hydrologic influence")
 
-filtered_dat <- rbind(filtered2_high, filtered2_med) 
+filtered_dat <- rbind(filtered2_high, filtered2_med)
 
 
 ## Visualize differences -------------------
@@ -92,3 +93,65 @@ png("figures/proportional_channel_length_dropnodes.png", width = 7, height = 9, 
 barplot_omr
 dev.off()
 
+## Look at map --------------------------
+# Visualization
+library(ggmap)
+library(ggspatial)
+library(deltamapr)
+library(viridis)
+library(sf)
+library(purrr)
+
+# Change all to 4326 (WGS)
+delta <- st_read("shapefiles/Bay_Delta_Poly_New.shp")
+delta_4326 <- st_transform(delta, crs = 4326) %>%
+  mutate(line = "analysis boundary")
+WW_Delta_4326 <- st_transform(WW_Delta, crs = st_crs(delta_4326))
+WW_Delta_crop <- st_crop(WW_Delta_4326,xmin = -122.2, xmax = -121, ymin = 37.5, ymax = 38.8) %>%
+  filter(HNAME!= "SAN FRANCISCO BAY")
+nodes <- st_read("shapefiles/nodes.shp") %>%
+  dplyr::select(node)
+nodes_4326 <- st_transform(nodes, crs = st_crs(delta_4326)) %>%
+  mutate(points = "DSM2 nodes")
+
+dataA <- inner_join(nodes_4326, filtered_dat_high)
+data <- dataA %>%
+  mutate(long = unlist(map(dataA$geometry,1)),
+         lat = unlist(map(dataA$geometry,2)))
+
+weird <- filter(data, long < -121.8) #nodes 432, 433, 434
+
+data2 <- inner_join(nodes_4326, filtered_dat_med)%>%
+  mutate(long = unlist(map(data2$geometry,1)),
+         lat = unlist(map(data2$geometry,2)))
+
+
+(map_omr_high <- ggplot() +
+     geom_sf(data = WW_Delta_crop, color = "gray94") +
+    geom_sf(data = delta_4326, aes(linetype = line), fill = NA, inherit.aes = FALSE)+
+  geom_sf(data = data, aes(color = OMR_Flow, shape = OMR_Flow), alpha = 0.3, size = 3,inherit.aes = FALSE) +
+    facet_wrap(~Group) +
+  labs(title = "Nodes with <0.25 similarity under different OMR Levels", color = "OMR",
+       shape = "OMR") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90)))
+
+png("figures/high_influence_map.png", width = 8, height = 6, units = "in", res = 300)
+map_omr_high
+dev.off()
+
+
+
+(map_omr_med <- ggplot() +
+    geom_sf(data = WW_Delta_crop, color = "gray94") +
+    geom_sf(data = delta_4326, aes(linetype = line), fill = NA, inherit.aes = FALSE)+
+    geom_sf(data = data2, aes(color = OMR_Flow, shape = OMR_Flow), alpha = 0.3, size = 3,inherit.aes = FALSE) +
+    facet_wrap(~Group) +
+
+    labs(title = "Nodes with 0.25-0.75 similarity under different OMR Levels", color = "OMR",
+         shape = "OMR") +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 90)))
+png("figures/med_influence_map.png", width = 8, height = 6, units = "in", res = 300)
+map_omr_med
+dev.off()
