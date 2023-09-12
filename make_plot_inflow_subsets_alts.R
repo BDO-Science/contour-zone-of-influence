149f05fce43fcd4dae82b1d7648d8f61014840dd
Cat Pien <catarina.pien@gmail.com>
2023-09-12 16:24:30 -0700

    Updated subset code for equal distributions of flow

--- make_plot_inflow_subsets_alts.R
+++ make_plot_inflow_subsets_alts.R
@@ -11,13 +11,16 @@ library(readr)
 # Read dat -------------------------------
 flow.dat <- read.csv("data_inflow/Inflow.csv") # This file will change based on new Calsim results
 flow.months <- flow.dat %>%
-  dplyr::filter(Month %in% c(12, 1, 2, 3, 4, 5, 6))
+  dplyr::filter(Month %in% c(12, 1, 2, 3, 4, 5, 6)) %>%
+  dplyr::filter(OMR>-6500)
 
 # New bins -----------------------------------------------
 
 # consider if we want to change this to bounds at 0 and 1?
-Sac.quant <- quantile(flow.months$Sac.cfs, probs = c(0.05, 0.25, 0.75, 0.95))
-Sjr.quant <- quantile(flow.months$SJR.cfs, probs = c(0.05, 0.25, 0.75, 0.95))
+Sac.quant <- quantile(flow.months$Sac.cfs, probs = c(0, 0.333, 0.666, 1))
+Sjr.quant <- quantile(flow.months$SJR.cfs, probs = c(0, 0.333, 0.666, 1))
+# Sac.quant <- quantile(flow.months$Sac.cfs, probs = c(0, 0.25, 0.75, 1))
+# Sjr.quant <- quantile(flow.months$SJR.cfs, probs = c(0, 0.25, 0.75, 1))
 
 # Rename values to make it easier to track
 Sac0 = Sac.quant[[1]]
@@ -33,15 +36,15 @@ SjrH = Sjr.quant[[4]]
 
 # Create groupings based on values above -------------------------------------
 Flow.subset <- flow.months %>%
-  mutate(lolo = if_else(Sac.cfs <= SacL & Sac.cfs >=Sac0 & SJR.cfs <=  SjrL & SJR.cfs >=  Sjr0, "lolo", ""),
-         medmed = if_else(Sac.cfs <= SacM & Sac.cfs >=SacL & SJR.cfs <=  SjrM & SJR.cfs >=  SjrL, "medmed", ""),
+  mutate(lolo = if_else(Sac.cfs < SacL & Sac.cfs >=Sac0 & SJR.cfs <  SjrL & SJR.cfs >=  Sjr0, "lolo", ""),
+         medmed = if_else(Sac.cfs < SacM & Sac.cfs >=SacL & SJR.cfs <  SjrM & SJR.cfs >=  SjrL, "medmed", ""),
          hihi = if_else(Sac.cfs <= SacH & Sac.cfs >=SacM & SJR.cfs <=  SjrH & SJR.cfs >=  SjrM, "hihi", ""),
-         himed = if_else(Sac.cfs <= SacH & Sac.cfs >=SacM & SJR.cfs <=  SjrM & SJR.cfs >=  SjrL, "himed", ""),
-         medhi = if_else(Sac.cfs <= SacM & Sac.cfs >=SacL & SJR.cfs <=  SjrH & SJR.cfs >=  SjrM, "medhi", ""),
-         lohi = if_else(Sac.cfs <= SacL & Sac.cfs >=Sac0 & SJR.cfs <=  SjrH & SJR.cfs >=  SjrM, "lohi", ""),
-         hilo = if_else(Sac.cfs <= SacH & Sac.cfs >=SacM & SJR.cfs <=  SjrL & SJR.cfs >=  Sjr0, "hilo", ""),
-         lomed = if_else(Sac.cfs <= SacL & Sac.cfs >=Sac0 & SJR.cfs <=  SjrM & SJR.cfs >=  SjrL, "lomed", ""),
-         medlo = if_else(Sac.cfs <= SacM& Sac.cfs >=SacL & SJR.cfs <=  SjrL & SJR.cfs >=  Sjr0, "medlo", ""))%>%
+         himed = if_else(Sac.cfs <= SacH & Sac.cfs >=SacM & SJR.cfs <  SjrM & SJR.cfs >=  SjrL, "himed", ""),
+         medhi = if_else(Sac.cfs < SacM & Sac.cfs >=SacL & SJR.cfs <=  SjrH & SJR.cfs >=  SjrM, "medhi", ""),
+         lohi = if_else(Sac.cfs < SacL & Sac.cfs >=Sac0 & SJR.cfs <=  SjrH & SJR.cfs >=  SjrM, "lohi", ""),
+         hilo = if_else(Sac.cfs <= SacH & Sac.cfs >=SacM & SJR.cfs <  SjrL & SJR.cfs >=  Sjr0, "hilo", ""),
+         lomed = if_else(Sac.cfs < SacL & Sac.cfs >=Sac0 & SJR.cfs <  SjrM & SJR.cfs >=  SjrL, "lomed", ""),
+         medlo = if_else(Sac.cfs < SacM& Sac.cfs >=SacL & SJR.cfs <  SjrL & SJR.cfs >=  Sjr0, "medlo", ""))%>%
   mutate(Sub.group = paste0(lolo, medmed, hihi, himed, medhi, lohi, hilo,lomed, medlo)) %>%
   filter(Sub.group != "") %>%
   arrange(OMR)
@@ -51,14 +54,32 @@ Flow.subset.clean <- Flow.subset %>%
   select(-lolo, -medmed, -hihi, -himed, -medhi, -lohi, -hilo, -lomed, -medlo) %>%
   filter(!(Sub.group %in% c("hilo", "lohi")))
 
-# Write file
-# write_csv(Flow.subset.clean, "data_export/flow_subsets_for_alts.csv")
+# Summarize sample size
+Flow.subset.summary <- Flow.subset %>%
+  group_by(Sub.group) %>%
+  summarize(n = n()) %>%
+  ungroup() %>%
+  mutate(Include = if_else(Sub.group %in% c("hilo", "lohi"), "N", "Y")) %>%
+  arrange(n) %>%
+  rename(Group = Sub.group)
 
+Flow.OMR.subset.summary <- Flow.subset %>%
+  group_by(Sub.group, OMR) %>%
+  summarize(n = n()) %>%
+  ungroup() %>%
+  mutate(Include = if_else(Sub.group %in% c("hilo", "lohi"), "N", "Y")) %>%
+  arrange(n) %>%
+  rename(Group = Sub.group)
+
+# Write file
+# write_csv(Flow.subset.clean, "data_export/flow_subsets_for_alts_equalspaced.csv")
+write_csv(Flow.subset.summary, "data_export/flow_subsets_samplesize_for_alts_equalspaced.csv")
+write_csv(Flow.OMR.subset.summary, "data_export/flow_omr_subsets_samplesize_for_alts_equalspaced.csv")
 
 # Remake the median plot ---------------------------------------------
 (plot_allvals <- ggplot() +
     geom_point(data = flow.months, aes(x=Sac.cfs, y=SJR.cfs), shape = 15, size = 2, color = "gray80") +
-    geom_point(data = Flow.subset %>%filter(OMR>-6500), aes(x = Sac.cfs, y = SJR.cfs, color = Sub.group, shape = factor(OMR)), size = 4) +
+    geom_point(data = Flow.subset.clean %>%filter(OMR>-6500), aes(x = Sac.cfs, y = SJR.cfs, color = Sub.group, shape = factor(OMR)), size = 4) +
     geom_vline(xintercept = SacMed, linetype = "dashed")+
     geom_hline(yintercept = SjrMed, linetype = "dashed")+
     scale_shape_manual(values = c(21, 22, 23)) +
@@ -69,10 +90,11 @@ Flow.subset.clean <- Flow.subset %>%
           axis.title = element_text(size = 11),
           legend.text = element_text(size = 11),
           legend.title = element_text(size = 12),
-          legend.position = "top"))
+          legend.position = "top",
+          legend.box = "vertical"))
 
 # Write plot
-png("figures/allflowvals_groups_updquant_plot.png", width = 8, height = 5, units = "in", res = 300, pointsize = 12)
+png("figures/allflowvals_groups_equalspaced_plot.png", width = 8, height = 5, units = "in", res = 300, pointsize = 12)
 plot_allvals
 dev.off()
 
@@ -88,7 +110,8 @@ Flowvals_word = data.frame(Group = c("lolo", "medmed", "hihi", "himed", "medhi",
   mutate(across(c(minSAC:maxSJR), ~round(., 1))) %>%
   mutate("Sac Flow Range (cfs)" = paste0(minSAC, "-", maxSAC),
          "SJR Flow Range (cfs)" = paste0(minSJR, "-", maxSJR)) %>%
-  dplyr::select(Group, Description, `Sac Flow Range (cfs)`, `SJR Flow Range (cfs)`)
+  dplyr::select(Group, Description, `Sac Flow Range (cfs)`, `SJR Flow Range (cfs)`) %>%
+  left_join(Flow.subset.summary %>% select(Group, n))
 
 # Clean table describing ranges for filtering data in code-------------------------------------
 Flowvals_program = data.frame(Group = c("lolo", "medmed", "hihi", "himed", "medhi", "lomed", "medlo"),
