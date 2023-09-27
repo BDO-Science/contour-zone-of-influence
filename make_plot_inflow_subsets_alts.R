f696f28d7ad30d4d4beb4ec481e59e04f451106f
Cat Pien <catarina.pien@gmail.com>
2023-09-27 14:17:14 -0700

    Updated code to include <-5500 OMR bin. Re-exported the summary stats and plot

--- make_plot_inflow_subsets_alts.R
+++ make_plot_inflow_subsets_alts.R
@@ -1,5 +1,5 @@
 ##############################################
-# Last updated 8/30/2023
+# Last updated 9/15/2023 by Catarina Pien (USBR)
 # Most up to date subsets
 # Creates a plot and two versions of the table to describe range of inflow values
 #################################################
@@ -9,27 +9,46 @@ library(dplyr)
 library(readr)
 library(readxl)
 library(lubridate)
+library(viridis)
+library(tidyr)
 
 # Read dat -------------------------------
 # flow.dat <- read.csv("data_inflow/Inflow.csv") # This file will change based on new Calsim results
 flow.dat0 <- read_excel("data_inflow/Reclamation_2021LTO_CalSim3_SacFreeport_SanJoaquinVernalis_rev01_20230913__NAA_090723.xlsx", skip = 6)
 colnames(flow.dat0) = c("Row", "Date", "Sac.cfs", "SJR.cfs")
-flow.dat <- flow.dat0 %>%
+flow.dat1 <- flow.dat0 %>%
   select(-Row) %>%
   mutate(Month = month(Date),
          Year = year(Date))
+# read omr data
+omr.dat <- read_excel("data_inflow/Reclamation_2021LTO_CalSim3_OMR_rev01_20230913__NAA_090723.xlsx", skip = 6)
+colnames(omr.dat) = c("Row", "Date", "OMR")
+# read export data
+export.dat <- read_excel("data_inflow/Reclamation_2021LTO_CalSim3_DeltaExports_rev01_20230926__NAA_090723.xlsx", skip = 6)
+colnames(export.dat) = c("Row", "Date", "Exports")
+# join all together and define OMR groupings
+flow.dat <- left_join(flow.dat1, omr.dat) %>%
+  left_join(export.dat) %>%
+  select(-Row) %>%
+  mutate(OMR_group = case_when(OMR >= -2500 & OMR <=-1500 ~ "-2000",
+                               OMR >= -4000 & OMR <=-3000 ~ "-3500",
+                               OMR >= -5500 & OMR <=-4500 ~ "-5000",
+                               OMR <=-5500 ~ "less than -5500",
+                               TRUE ~ "Other"))
 
+# Sample sizes and summaries ----------------------
+(flow.dat.summary <- flow.dat %>%
+  group_by(OMR_group) %>%
+  summarize(n =n()))
 
+# Filter down to Dec-June
 flow.months <- flow.dat %>%
   dplyr::filter(Month %in% c(12, 1, 2, 3, 4, 5, 6))
 
-# New bins -----------------------------------------------
+# Add inflow bins -----------------------------------------------
 
-# consider if we want to change this to bounds at 0 and 1?
 Sac.quant <- quantile(flow.months$Sac.cfs, probs = c(0, 0.333, 0.666, 1))
 Sjr.quant <- quantile(flow.months$SJR.cfs, probs = c(0, 0.333, 0.666, 1))
-# Sac.quant <- quantile(flow.months$Sac.cfs, probs = c(0, 0.25, 0.75, 1))
-# Sjr.quant <- quantile(flow.months$SJR.cfs, probs = c(0, 0.25, 0.75, 1))
 
 # Rename values to make it easier to track
 Sac0 = Sac.quant[[1]]
@@ -43,8 +62,8 @@ SjrL = Sjr.quant[[2]]
 SjrM = Sjr.quant[[3]]
 SjrH = Sjr.quant[[4]]
 
-# Create groupings based on values above -------------------------------------
-Flow.subset <- flow.months %>%
+# Create groupings based on values above
+Flow.subsetall  <- flow.months %>%
   mutate(lolo = if_else(Sac.cfs < SacL & Sac.cfs >=Sac0 & SJR.cfs <  SjrL & SJR.cfs >=  Sjr0, "lolo", ""),
          medmed = if_else(Sac.cfs < SacM & Sac.cfs >=SacL & SJR.cfs <  SjrM & SJR.cfs >=  SjrL, "medmed", ""),
          hihi = if_else(Sac.cfs <= SacH & Sac.cfs >=SacM & SJR.cfs <=  SjrH & SJR.cfs >=  SjrM, "hihi", ""),
@@ -53,45 +72,85 @@ Flow.subset <- flow.months %>%
          lohi = if_else(Sac.cfs < SacL & Sac.cfs >=Sac0 & SJR.cfs <=  SjrH & SJR.cfs >=  SjrM, "lohi", ""),
          hilo = if_else(Sac.cfs <= SacH & Sac.cfs >=SacM & SJR.cfs <  SjrL & SJR.cfs >=  Sjr0, "hilo", ""),
          lomed = if_else(Sac.cfs < SacL & Sac.cfs >=Sac0 & SJR.cfs <  SjrM & SJR.cfs >=  SjrL, "lomed", ""),
-         medlo = if_else(Sac.cfs < SacM& Sac.cfs >=SacL & SJR.cfs <  SjrL & SJR.cfs >=  Sjr0, "medlo", ""))%>%
-  mutate(Sub.group = paste0(lolo, medmed, hihi, himed, medhi, lohi, hilo,lomed, medlo)) %>%
-  filter(Sub.group != "")
+         medlo = if_else(Sac.cfs < SacM& Sac.cfs >=SacL & SJR.cfs <  SjrL & SJR.cfs >=  Sjr0, "medlo", "")) %>%
+  mutate(Sub.group = paste0(lolo, medmed, hihi, himed, medhi, lohi, hilo,lomed, medlo))
 
 # Remove extra variables
-Flow.subset.clean <- Flow.subset %>%
+Flow.subset.clean <- Flow.subsetall %>%
+  filter(Sub.group != "") %>%
+  filter(OMR_group!="Other") %>%
   select(-lolo, -medmed, -hihi, -himed, -medhi, -lohi, -hilo, -lomed, -medlo)
 
-# Summarize sample size
-Flow.subset.summary <- Flow.subset %>%
+# Summarize sample sizes -----------------------------------------------------
+Flow.subset.sumstats <- Flow.subsetall %>%
+  group_by(Sub.group) %>%
+  summarize(meanOMR = round(mean(OMR),0),
+            meanExports = round(mean(Exports),0),
+            n = n()) %>%
+  rename(Group = Sub.group)
+
+# sample sizes for inflow groups
+Flow.subset.summary <- Flow.subset.clean %>%
   group_by(Sub.group) %>%
   summarize(n = n()) %>%
   ungroup() %>%
-  mutate(Include = if_else(Sub.group %in% c("hilo", "lohi"), "N", "Y")) %>%
   arrange(n) %>%
-  rename(Group = Sub.group)
+  rename(`Inflow Group` = Sub.group)
 
-Flow.OMR.subset.summary <- Flow.subset %>%
-  group_by(Sub.group, OMR) %>%
-  summarize(n = n()) %>%
+# sample sizes for inflow-omr groups
+Flow.OMR.subset.summary <- Flow.subset.clean %>%
+  group_by(Sub.group, OMR_group) %>%
+  summarize(n = n(),
+            meanOMR = round(mean(OMR), 0),
+            meanExports = round(mean(Exports),0)) %>%
   ungroup() %>%
-  mutate(Include = if_else(Sub.group %in% c("hilo", "lohi"), "N", "Y")) %>%
-  arrange(n) %>%
-  rename(Group = Sub.group)
+  mutate(Include = if_else(n<5, "N", "Y"),
+         OMR_Range = if_else(OMR_group == "-2000", "-2500 to -1500 cfs",
+                             if_else(OMR_group == "-3500", "-4000 to -3000 cfs",
+                             if_else(OMR_group == "-5000", "-5500 to -4500 cfs",
+                            if_else(OMR_group == "less than -5500", "less than -5500", ""))))) %>%
+  arrange(Sub.group, OMR_group) %>%
+  rename(`Inflow Group` = Sub.group) %>%
+  select(`Inflow Group`,
+         `OMR Group` = OMR_group,
+         OMR_Range,
+         `Mean OMR (cfs)` = meanOMR,
+         ` Mean Exports (cfs)` = meanExports,
+         n)
+
+Flow.OMR.complete <- Flow.OMR.subset.summary %>%
+  complete(`Inflow Group`,
+           nesting(`OMR Group`, `OMR_Range`),
+           fill = list(n = 0))
 
 # Write file
-write_csv(Flow.subset.clean, "data_export/flow_subsets_month_year_NAA.csv")
-write_csv(Flow.subset.summary, "data_export/flow_subsets_samplesize_NAA_equalspaced.csv")
-write_csv(Flow.OMR.subset.summary, "data_export/flow_omr_subsets_samplesize_for_alts_equalspaced.csv")
+# write_csv(Flow.subset.clean, "data_export/flow_subsets_month_year_NAA.csv")
+# write_csv(Flow.subset.summary, "data_export/flow_subsets_samplesize_NAA_equalspaced.csv")
+write_csv(Flow.OMR.complete, "data_export/flow_omr_samplesizes.csv")
 
-# Remake the median plot ---------------------------------------------
+
+# Plot of OMR and Exports
+ggplot(Flow.subset.clean) +
+  geom_point(aes(OMR, Exports, color = Sub.group), size = 3)
+
+# Look at stormflex
+Stormflex.data <- Flow.subsetall %>%
+  filter(OMR_group == "less than -5500") %>%
+  select(-lolo, -medmed, -hihi, -himed, -medhi, -lohi, -hilo, -lomed, -medlo)
+
+ggplot(Stormflex.data) + geom_point(aes(Sac.cfs, SJR.cfs, color = Sub.group), size = 5) + theme_bw()
+
+
+
+# Plot of inflow and OMR groupings ---------------------------------------------
 (plot_allvals <- ggplot() +
     geom_point(data = flow.months, aes(x=Sac.cfs, y=SJR.cfs), shape = 15, size = 2, color = "gray80") +
-    geom_point(data = Flow.subset.clean , aes(x = Sac.cfs, y = SJR.cfs, color = Sub.group), size = 4) +
+    geom_point(data = Flow.subset.clean , aes(x = Sac.cfs, y = SJR.cfs, color = Sub.group, shape = OMR_group), size = 4) +
     geom_vline(xintercept = SacMed, linetype = "dashed")+
     geom_hline(yintercept = SjrMed, linetype = "dashed")+
-    scale_shape_manual(values = c(21, 22, 23)) +
+    scale_shape_manual(values = c(21, 22, 23, 24, 25)) +
     scale_color_viridis_d(option = "turbo")+
-    labs(x = "Sac Flow (cfs)", y = "SJR Flow (cfs)", color = "Inflow Group") +
+    labs(x = "Sacramento Inflow (cfs)", y = "San Joaquin Inflow (cfs)", color = "Inflow Group", shape = "OMR") +
     theme_bw() +
     theme(axis.text = element_text(size = 12),
           axis.title = element_text(size = 11),
@@ -100,40 +159,68 @@ write_csv(Flow.OMR.subset.summary, "data_export/flow_omr_subsets_samplesize_for_
           legend.position = "top",
           legend.box = "vertical"))
 
+(plot_zoom <- ggplot() +
+    geom_point(data = flow.months, aes(x=Sac.cfs, y=SJR.cfs), shape = 15, size = 2, color = "gray80") +
+    geom_point(data = Flow.subset.clean , aes(x = Sac.cfs, y = SJR.cfs, color = Sub.group, shape = OMR_group), size = 4) +
+    geom_vline(xintercept = SacMed, linetype = "dashed")+
+    geom_hline(yintercept = SjrMed, linetype = "dashed")+
+    scale_shape_manual(values = c(21, 22, 23, 24, 25)) +
+    scale_color_viridis_d(option = "turbo")+
+    ylim(800,5000) +
+    xlim(5500, 26000) +
+    labs(x = "Sacramento Inflow (cfs)", y = "San Joaquin Inflow (cfs)", color = "Inflow Group", shape = "OMR") +
+    theme_bw() +
+    theme(axis.text = element_text(size = 12),
+          axis.title = element_text(size = 11),
+          legend.text = element_text(size = 11),
+          legend.title = element_text(size = 12),
+          legend.position = "none",
+          legend.box = "vertical"))
+
+# Combine zoom and main plot
+library(patchwork)
+(plot <- plot_allvals + inset_element(plot_zoom, left = 0.02, bottom = 0.55, right = 0.6, top = 0.97))
+
 # Write plot
-png("figures/allflowvals_groups_NAA_equalspaced_plot.png", width = 8, height = 5, units = "in", res = 300, pointsize = 12)
-plot_allvals
+png("figures/allflowvals_groups_NAA_OMR_plot.png", width = 9, height = 7, units = "in", res = 300, pointsize = 12)
+plot
 dev.off()
 
-# Clean table describing ranges for a word document ----------------------------------------
-Flowvals_word = data.frame(Group = c("lolo", "medmed", "hihi", "himed", "medhi", "lomed", "medlo", "lohi", "hilo"),
-                      Description = c("Low SAC Low SJR", "Med SAC Med SJR", "High SAC High SJR",
-                                      "High SAC Med SJR", "Med SAC High SJR",
-                                      "Low SAC Med SJR", "Med SAC Low SJR", "Low SAC High SJR", "High SAC Low SJR"),
-                      minSAC = c(Sac0, SacL, SacM, SacM, SacL, Sac0, SacL, Sac0, SacM),
-                      maxSAC = c(SacL, SacM, SacH, SacH, SacM, SacL, SacM, SacL, SacH),
-                      minSJR = c(Sjr0, SjrL, SjrM, SjrL, SjrM, SjrL, Sjr0, SjrM, Sjr0),
-                      maxSJR = c(SjrL, SjrM, SjrH, SjrM, SjrH, SjrM, SjrL, SjrH, SjrL)) %>%
-  mutate(across(c(minSAC:maxSJR), ~round(., 1))) %>%
-  mutate("Sac Flow Range (cfs)" = paste0(minSAC, "-", maxSAC),
-         "SJR Flow Range (cfs)" = paste0(minSJR, "-", maxSJR)) %>%
-  dplyr::select(Group, Description, `Sac Flow Range (cfs)`, `SJR Flow Range (cfs)`) %>%
-  left_join(Flow.subset.summary %>% select(Group, n))
+
+# Clean table for summary statistics --------------------------------------------------------
+# This one is currently in the appendix
+Flowvals_unfiltered = data.frame(Group = c("lolo", "medmed", "hihi", "himed", "medhi", "lomed", "medlo", "lohi", "hilo"),
+                           Description = c("Low SR Low SJR", "Med SR Med SJR", "High SR High SJR",
+                                           "High SR Med SJR", "Med SR High SJR",
+                                           "Low SR Med SJR", "Med SR Low SJR", "Low SR High SJR", "High SR Low SJR"),
+                           minSAC = c(Sac0, SacL, SacM, SacM, SacL, Sac0, SacL, Sac0, SacM),
+                           maxSAC = c(SacL, SacM, SacH, SacH, SacM, SacL, SacM, SacL, SacH),
+                           minSJR = c(Sjr0, SjrL, SjrM, SjrL, SjrM, SjrL, Sjr0, SjrM, Sjr0),
+                           maxSJR = c(SjrL, SjrM, SjrH, SjrM, SjrH, SjrM, SjrL, SjrH, SjrL)) %>%
+  mutate(across(c(minSAC:maxSJR), ~round(., 0))) %>%
+  mutate("SR Flow Range (cfs)" = paste0(minSAC, " - ", maxSAC),
+         "SJR Flow Range (cfs)" = paste0(minSJR, " - ", maxSJR)) %>%
+  dplyr::select(Group, Description, `SR Flow Range (cfs)`, `SJR Flow Range (cfs)`) %>%
+  left_join(Flow.subset.sumstats %>% select(Group, meanOMR, meanExports, n)) %>%
+  select(`Inflow Group` = Group,
+         Description,
+         `SR Flow Range (cfs)`,
+         `SJR Flow Range (cfs)`,
+         `Mean OMR (cfs)` = meanOMR,
+         `Mean Exports (cfs)` = meanExports,
+         n)
 
 # Clean table describing ranges for filtering data in code-------------------------------------
 Flowvals_program = data.frame(Group = c("lolo", "medmed", "hihi", "himed", "medhi", "lomed", "medlo", "lohi", "hilo"),
-                              Description = c("Low SAC Low SJR", "Med SAC Med SJR", "High SAC High SJR",
-                                              "High SAC Med SJR", "Med SAC High SJR",
-                                              "Low SAC Med SJR", "Med SAC Low SJR", "Low SAC High SJR", "High SAC Low SJR"),
+                              Description = c("Low SR Low SJR", "Med SR Med SJR", "High SR High SJR",
+                                              "High SR Med SJR", "Med SR High SJR",
+                                              "Low SR Med SJR", "Med SR Low SJR", "Low SR High SJR", "High SR Low SJR"),
                               minSAC = c(Sac0, SacL, SacM, SacM, SacL, Sac0, SacL, Sac0, SacM),
                               maxSAC = c(SacL, SacM, SacH, SacH, SacM, SacL, SacM, SacL, SacH),
                               minSJR = c(Sjr0, SjrL, SjrM, SjrL, SjrM, SjrL, Sjr0, SjrM, Sjr0),
                               maxSJR = c(SjrL, SjrM, SjrH, SjrM, SjrH, SjrM, SjrL, SjrH, SjrL)) %>%
   mutate(across(c(minSAC:maxSJR), ~round(., 1))) %>%
-  # mutate("Sac Flow Range (cfs)" = paste0(minSAC, "-", maxSAC),
-         # "SJR Flow Range (cfs)" = paste0(minSJR, "-", maxSJR)) %>%
   dplyr::select(Group, Description,minSAC, maxSAC, minSJR, maxSJR)
 
 # Write tables
-write_csv(Flowvals_word, "data_export/flow_subset_table_NAA_for_alts.csv")
-# write_csv(Flowvals_program, "data_export/flow_subset_table_forsteve_for_alts.csv")
+write_csv(Flowvals_unfiltered, "data_export/flow_subset_table_sumstats.csv")
