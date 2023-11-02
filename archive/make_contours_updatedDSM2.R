767234854f0f8afa90671fcf0c9f02a3df29e6c2
Cat Pien <catarina.pien@gmail.com>
2023-11-02 15:48:40 -0700

    moved old files to archive

---
+++ archive/make_contours_updatedDSM2.R
@@ -0,0 +1,280 @@
+# make_contours.R ######
+#### Catarina Pien and Lisa Elliott
+
+# This code uses zone of influence modeling results (DSM2) to create contours showing
+#  how zone of influence changes from operational facilities based on pumping
+#  Contour lines of a specific level are then compared between different flow levels
+#  to indicate how changing OMR will influence the zone of influence.
+
+# Packages
+# general
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(lubridate)
+library(knitr)
+library(tidyr)
+library(sf)
+library(stars)
+library(readr)
+library(janitor)
+
+#IDW
+library(sp)
+library(gstat)
+library(raster)
+library(tmap)
+library(rgdal)
+
+# Visualization
+library(ggmap)
+library(ggspatial)
+library(deltamapr)
+library(viridis)
+
+# Read/Join data ---------------------------------------------------------
+delta <- st_read("shapefiles/Bay_Delta_Poly_New.shp")
+zoi_file_NAA = list.files("data_updatedDSM2", pattern = "NAA_.*csv$", full.names = TRUE)
+#zoi_file_D1641
+zoi_data <- lapply(zoi_file_NAA, read_csv) %>%
+  bind_rows(.id = "id") %>%
+  mutate(id = substr(zoi_file_NAA[as.numeric(id)], 41, 44)) %>%
+  rename(Flow = id)
+nodes <- st_read("shapefiles/nodes.shp") %>%
+  dplyr::select(node)
+channels <- read_csv("data_updatedDSM2/Reclamation_2021LTO_DSM2_Version806_ChannelLengths.csv") %>%
+  janitor::clean_names()  %>%
+  rename(channel_number = chan_no)
+
+# Drop nodes that are causing issues
+dropNodes <- c(146, 147, 148, 206, 242, 246)
+nodes <- nodes[!nodes$node %in% dropNodes, ]
+
+# Join channel lengths with zoi data
+zoi_channel <- left_join(zoi_data, channels)
+
+# Change all to 4326 (WGS)
+delta_4326 <- st_transform(delta, crs = 4326) %>%
+  mutate(line = "analysis boundary")
+nodes_4326 <- st_transform(nodes, crs = st_crs(delta_4326)) %>%
+  mutate(points = "DSM2 nodes")
+WW_Delta_4326 <- st_transform(WW_Delta, crs = st_crs(delta_4326))
+WW_Delta_crop <- st_crop(WW_Delta_4326,xmin = -122.2, xmax = -121, ymin = 37.5, ymax = 38.8) %>%
+  filter(HNAME!= "SAN FRANCISCO BAY")
+plot(WW_Delta_crop)
+
+# Look at availability of data
+
+
+
+# Functions ------------------------------------
+
+# Create data frame for each month and flow level
+create_df <- function(month, flow) {
+  month_data <- zoi_channel %>%
+    dplyr::select(Flow, node, channel_number, length_feet, upnode, downnode, month) %>%
+    rename(DSM2 = month)
+  month_node <- inner_join(nodes_4326, month_data) %>%
+    mutate(DSM2 = ifelse(DSM2<0, NA, DSM2),
+           Month = month) %>%
+    na.omit()# may be some missing
+
+  df_filtered <- month_node %>% filter(Flow == flow)
+  df_filt_sp <- as(df_filtered, "Spatial")
+
+  return(df_filtered)
+  #return(df_filt_sp)
+}
+
+# Filter to correct similarity level (e.g. 0.7)
+f_filter_similarity <- function(df) {
+  # figure out the downstream nodes that correspond to upstream %sim of 0.7 or greater
+  tokeep <- df %>%
+  mutate(include1 = ifelse(DSM2 < 0.7, "Y", "N")) %>%
+  filter(include1 == "Y") %>%
+  pivot_longer(cols = c(upnode, downnode), values_to = "nodestokeep") %>%
+  st_drop_geometry()
+  nodestokeep <- unique(tokeep$nodestokeep)
+
+  # do the actual filtering of nodes
+  filtered_month <- df %>%
+    filter(node %in% nodestokeep)
+
+  # filtered_month <- df %>%
+  #   filter(DSM2 < 0.7)
+
+  return(filtered_month)
+}
+
+
+# Filter to correct similarity level (e.g. 0.7)
+f_filter_similarity2 <- function(df) {
+
+
+  filtered_month <- df %>%
+  filter(DSM2 < 0.7)
+
+  return(filtered_month)
+}
+
+# Analysis ------------------------------
+
+## Create data frames --------------------
+
+### June --------------
+jun_2000_sp <- create_df(month = "Jun", flow = 2000)
+jun_3500_sp <- create_df(month = "Jun", flow = 3500)
+jun_5000_sp <- create_df(month = "Jun", flow = 5000)
+#jun_6500_sp <- create_df(month = "Jun", flow = 6500)
+
+### May --------------
+may_2000_sp <- create_df(month = "May", flow = 2000)
+may_3500_sp <- create_df(month = "May", flow = 3500)
+#may_5000_sp <- create_df(month = "May", flow = 5000)
+#may_6500_sp <- create_df(month = "May", flow = 6500)
+
+### April --------------
+apr_2000_sp <- create_df(month = "Apr", flow = 2000)
+apr_3500_sp <- create_df(month = "Apr", flow = 3500)
+#apr_5000_sp <- create_df(month = "Apr", flow = 5000)
+#apr_6500_sp <- create_df(month = "Apr", flow = 6500)
+
+### March --------------
+mar_2000_sp <- create_df(month = "Mar", flow = 2000)
+mar_3500_sp <- create_df(month = "Mar", flow = 3500)
+mar_5000_sp <- create_df(month = "Mar", flow = 5000)
+#mar_6500_sp <- create_df(month = "Mar", flow = 6500)
+
+### February --------------
+feb_2000_sp <- create_df(month = "Feb", flow = 2000)
+feb_3500_sp <- create_df(month = "Feb", flow = 3500)
+feb_5000_sp <- create_df(month = "Feb", flow = 5000)
+#feb_6500_sp <- create_df(month = "Feb", flow = 6500)
+
+### January --------------
+jan_2000_sp <- create_df(month = "Jan", flow = 2000)
+jan_3500_sp <- create_df(month = "Jan", flow = 3500)
+jan_5000_sp <- create_df(month = "Jan", flow = 5000)
+#jan_6500_sp <- create_df(month = "Jan", flow = 6500)
+
+### December --------------
+dec_2000_sp <- create_df(month = "Dec", flow = 2000)
+dec_3500_sp <- create_df(month = "Dec", flow = 3500)
+dec_5000_sp <- create_df(month = "Dec", flow = 5000)
+dec_6500_sp <- create_df(month = "Dec", flow = 6500)
+
+# Check spatial distribution-------------------------------------------------
+
+## Looks like we are already inside boundary
+ggplot() +
+  geom_sf(data = delta_4326, aes(linetype = line), fill = NA, inherit.aes = FALSE)+
+  geom_sf(data = dec_2000_sp, aes(color = Flow), inherit.aes = FALSE)
+
+# Filter to proportion of similarity ----------------------------------
+
+## List all the files --------------------------
+df <- list(dec_2000_sp, dec_3500_sp, dec_5000_sp, dec_6500_sp,
+           jan_2000_sp, jan_3500_sp, jan_5000_sp,
+           feb_2000_sp, feb_3500_sp, feb_5000_sp,
+           mar_2000_sp, mar_3500_sp, mar_5000_sp,
+           apr_2000_sp, apr_3500_sp,
+           may_2000_sp, may_3500_sp,
+           jun_2000_sp, jun_3500_sp, jun_5000_sp)
+
+## Loop it ---------------------------------------
+# filtered <- lapply(df, f_filter_similarity) %>%
+#   bind_rows() %>%
+#   mutate(Flow = as.factor(as.numeric(Flow) * -1))
+
+### This one should be filtering correctly
+filtered_try2 <- lapply(df, f_filter_similarity2) %>%
+  bind_rows() %>%
+  mutate(Flow = as.factor(as.numeric(Flow) * -1))
+
+# filtered_try2 <- list()
+# for(r in 1:nrow(df)){
+#   filtered_try2[r] <- filter(as.data.frame(df[r]), DSM2 < 0.7)
+# }
+
+# Calculate total length -------------------------
+filtered2 <- filtered_try2 %>%
+  group_by(Month, Flow) %>%
+  summarize(sumLength = sum(length_feet))%>%
+  ungroup() %>%
+  mutate(Month = factor(Month, levels = c("Dec", "Jan", "Feb", "Mar", "Apr", "May", "Jun")))
+
+# Visualize -------------------------------------
+
+## Check map (needs work) -----------------------------------
+
+map_omr_vals <- ggplot() +
+  geom_sf(data = delta_4326, aes(linetype = line), fill = NA, inherit.aes = FALSE)+
+  geom_sf(data = filtered2, aes(color = Flow, shape = Flow), alpha = 0.3, inherit.aes = FALSE) +
+  facet_wrap(~Month) +
+  labs(title = "Nodes with <0.7 similarity under different OMR Levels", color = "OMR",
+       shape = "OMR") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90))
+
+png(filename = here::here("figures", "plot_map_omr_vals.png"),
+    width = 6, height = 6, units = "in",
+    pointsize = 12, family = "sans", res = 300)
+map_omr_vals
+dev.off()
+
+## Visualize differences -------------------
+barplot_omr <- ggplot(filtered2) +
+  geom_col(aes(Month, sumLength, fill = Flow), position = "dodge2")  +
+  labs(y = "Sum Channel Length") +
+  viridis::scale_fill_viridis(discrete = TRUE) +
+  theme_bw()
+
+# png(filename = here::here("figures", "plot_barplot_omr_channellength.png"),
+#     width = 6, height = 6, units = "in",
+#     pointsize = 12, family = "sans", res = 300)
+# barplot_omr
+# dev.off()
+
+
+### Look at February
+feb <- filtered_try2%>%
+  filter(Month == "Feb")
+
+feb_wide <- feb %>%
+  select(-upnode, -channel_number) %>%
+  pivot_wider( values_from= c("length_feet","DSM2"), names_from = "Flow")
+
+feb235 <- feb_2000_sp %>%
+  st_drop_geometry() %>%
+  select(-points, -channel_number, -Flow) %>%
+  rename(DSM2_2000 =DSM2) %>%
+  full_join(feb_3500_sp %>% st_drop_geometry(), by = c("upnode", "downnode", "Month", "node")) %>%
+  select(-points, -channel_number, -Flow) %>%
+  rename(DSM2_3500 = DSM2) %>%
+  full_join(feb_5000_sp %>% st_drop_geometry(), by = c("upnode", "downnode", "Month", "node")) %>%
+  st_drop_geometry() %>%
+  rename(DSM2_5000 = DSM2) %>%
+  select(-points, -channel_number, -Flow) %>%
+  mutate(diff = ifelse(DSM2_3500 <0.7 & DSM2_5000 >=0.7, "diff", "0")) %>%
+  #mutate(diff = ifelse(DSM2_3500 < DSM2_5000, "diff", "0")) %>%
+  mutate(DSM2_3500 = round(DSM2_3500, 3),
+         DSM2_5000 = round(DSM2_5000, 3)) %>%
+  select(node, upnode, downnode, length_2000 = length_feet.y, length_3500 = length_feet.x,
+         length_5000 = length_feet, DSM2_2000, DSM2_3500, DSM2_5000, diff)
+
+######### Cat ended here ##############################
+
+# TO DO
+
+# To check we are including the right nodes,
+  # map which ones are truly <0.7 and which ones are not and
+  # see they are next to each other.
+# Think about why 3500 seems to be greater than 5000.
+# Do we need to draw lines at all?
+# Export table
+
+
+# Total delta outflow could be different even though OMR is the same
+# Standardize ZOI by inflow
+# Across a range of inflows and OMR flows how does the ZOI change
+# We don't pump this negative when we could have a larger ZOI
