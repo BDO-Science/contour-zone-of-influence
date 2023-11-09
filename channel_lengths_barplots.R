e487935500f4929c28e1861a572cf6bc8dbec74e
Cat Pien <catarina.pien@gmail.com>
2023-11-09 14:25:28 -0800

    cleaned up channel lengths barplot code, wrote out updated barplots

--- channel_lengths_barplots.R
+++ channel_lengths_barplots.R
@@ -2,7 +2,7 @@
 ##### Catarina Pien and Lisa Elliott #####
 # This code uses zone of influence modeling results (DSM2) to create barplots showing
 # how channel length associated with the zone of influence changes from operational facilities based on pumping
-# to different OMR levels, at different SJR and SR flow levels
+# to different OMR levels, at different SJR and SR flow levels and alternatives
 
 # Packages
 # general
@@ -28,6 +28,7 @@ alt_order = c("NAA","Alt1","Alt2woTUCPwoVA","Alt2woTUCPDeltaVA", "Alt2woTUCPAllV
 # from contour_maps_inflow_allalts.R
 zoi_channel_group <- read_csv("data_export/prop_overlap_data_long.csv") %>%
   filter(overlap>=0)
+n_flow_OMR_alt <- read_csv("data_export/samplesizes_flow_OMR_alt.csv")
 
 # Filter to contours of interest -------------------------------------
 # Based on BA
@@ -41,15 +42,15 @@ filtered_dat_low <- zoi_channel_group %>%
   filter(overlap > 0.75)
 
 
-# Calculate total length -------------------------
+# Calculate total length for each group -------------------------
 filtered2_high <- filtered_dat_high %>%
   group_by(group, OMR_Flow, Alt) %>%
   summarize(sumLength = sum(length))%>%
   ungroup() %>%
   mutate(group = factor(group, levels = c("lolo", "lomed", "lohi", "medlo", "medmed", "medhi", "hilo", "himed", "hihi")))%>%
-  mutate(Alt = case_when(Alt == "Alt2d" ~ "Alt2woTUCPwoVA",
-                         Alt == "Alt2b" ~ "Alt2woTUCPDeltaVA",
-                         Alt == "Alt2c" ~ "Alt2woTUCPAllVA",
+  mutate(Alt = case_when(Alt == "Alt2b" ~ "Alt2woTUCPwoVA",
+                         Alt == "Alt2c" ~ "Alt2woTUCPDeltaVA",
+                         Alt == "Alt2d" ~ "Alt2woTUCPAllVA",
                          Alt == "Alt2a" ~ "Alt2wTUCPwoVA",
                          TRUE ~ Alt),
          Alt = factor(Alt, levels = alt_order)) %>%
@@ -61,9 +62,9 @@ filtered2_med <- filtered_dat_med %>%
   summarize(sumLength = sum(length))%>%
   ungroup() %>%
   mutate(group = factor(group, levels = c("lolo", "lomed", "lohi", "medlo", "medmed", "medhi", "hilo", "himed", "hihi"))) %>%
-  mutate(Alt = case_when(Alt == "Alt2d" ~ "Alt2woTUCPwoVA",
-                         Alt == "Alt2b" ~ "Alt2woTUCPDeltaVA",
-                         Alt == "Alt2c" ~ "Alt2woTUCPAllVA",
+  mutate(Alt = case_when(Alt == "Alt2b" ~ "Alt2woTUCPwoVA",
+                         Alt == "Alt2c" ~ "Alt2woTUCPDeltaVA",
+                         Alt == "Alt2d" ~ "Alt2woTUCPAllVA",
                          Alt == "Alt2a" ~ "Alt2wTUCPwoVA",
                          TRUE ~ Alt),
          Alt = factor(Alt, levels = alt_order)) %>%
@@ -76,27 +77,31 @@ filtered2_low <- filtered_dat_low %>%
   summarize(sumLength = sum(length))%>%
   ungroup() %>%
   mutate(group = factor(group, levels = c("lolo", "lomed", "lohi", "medlo", "medmed", "medhi", "hilo", "himed", "hihi"))) %>%
-  mutate(Alt = case_when(Alt == "Alt2d" ~ "Alt2woTUCPwoVA",
-                         Alt == "Alt2b" ~ "Alt2woTUCPDeltaVA",
-                         Alt == "Alt2c" ~ "Alt2woTUCPAllVA",
+  mutate(Alt = case_when(Alt == "Alt2b" ~ "Alt2woTUCPwoVA",
+                         Alt == "Alt2c" ~ "Alt2woTUCPDeltaVA",
+                         Alt == "Alt2d" ~ "Alt2woTUCPAllVA",
                          Alt == "Alt2a" ~ "Alt2wTUCPwoVA",
                          TRUE ~ Alt),
          Alt = factor(Alt, levels = alt_order)) %>%
   mutate(pLength = sumLength/total_channel_length) %>%
   mutate(h_influence = "Low hydrologic influence")
 
-# combine files and rename to  official names
+#* Combine files and rename to  official names ---------------------------
 filtered_dat <- rbind(filtered2_high, filtered2_med, filtered2_low) %>%
   mutate(h_influence = factor(h_influence, levels = c("Low hydrologic influence", "Medium hydrologic influence", "High hydrologic influence")),
-        Alt = case_when(Alt == "Alt2d" ~ "Alt2woTUCPwoVA",
-                        Alt == "Alt2b" ~ "Alt2woTUCPDeltaVA",
-                        Alt == "Alt2c" ~ "Alt2woTUCPAllVA",
+        Alt = case_when(Alt == "Alt2b" ~ "Alt2woTUCPwoVA",
+                        Alt == "Alt2c" ~ "Alt2woTUCPDeltaVA",
+                        Alt == "Alt2d" ~ "Alt2woTUCPAllVA",
                         Alt == "Alt2a" ~ "Alt2wTUCPwoVA",
                         TRUE ~ Alt),
         Alt = factor(Alt, levels = alt_order))
 
-## Visualize differences -------------------
+filtered_dat_BA <- filtered_dat %>%
+  filter(!(Alt %in% c("Alt1", "Alt4", "Alt2wTUCPwoVA")))
+
+# Visualize differences -------------------
 pal <- c('#9a3324', "#88CCEE","#AA4499",'#003E51','#007396', '#C69214', '#DDCBA4','#FF671F', '#215732','#4C12A1')
+
 # unstacked
 barplot_omr <- ggplot(filtered_dat) +
   geom_col(aes(Alt, pLength, fill = OMR_Flow), position = "dodge2")  +
@@ -106,30 +111,77 @@ barplot_omr <- ggplot(filtered_dat) +
   theme_bw()
 barplot_omr
 
-# look at just medium
-med_barplot <- filtered2_med %>%
-  ggplot() +
-  geom_col(aes(OMR_Flow, pLength, fill = Alt), position= "dodge2") +
-  facet_wrap(~group) +
-  labs(y = "Proportional Channel Length", title = "Medium hydro influence") +
-  theme_bw() +
-  scale_fill_manual(values = pal[c(3,4,5,6,7,8,10)])
-ggsave(filename="figures/attachment_plots/med_influence_omr_barplots.png", plot=med_barplot, height = 6, width = 7, units = "in")
+#* look at just medium -------------------
 
-(med_barplot2 <- filtered2_med %>%
+# Add in sample size
+med_barplot_data <- left_join(filtered2_med, n_flow_OMR_Alt %>% dplyr::select(OMR_Flow = OMR, Alt, group = Sub.group, n)) %>%
+  mutate(low_n = if_else(n<5, "*", "")) %>%
+  complete(OMR_Flow, group, nesting(Alt)) %>%
+  filter(group!="NA")
+
+#** EIS figures ---------------
+(med_barplot_eis <- med_barplot_data %>%
   ggplot() +
-  geom_col(aes(group, pLength, fill = Alt), position= "dodge2") +
-  facet_wrap(~OMR_Flow) +
-  labs(y = "Proportional Channel Length", x = "Inflow Group", title = "Medium hydro influence") +
-  theme_bw() +
-    theme(axis.text.x = element_text(angle = 90)) +
+  geom_col(aes(OMR_Flow, pLength, fill = Alt), position= position_dodge(0.75, preserve = "single"), width = 0.6) +
+  geom_text(aes(OMR_Flow, pLength, group = Alt, label = low_n), vjust = -0.01, position= position_dodge(0.75))+
+  facet_wrap(~group) +
+  labs(y = "Proportional Channel Length", x = "OMR Bin", fill = "Alternative") +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 90),
+          legend.position = "top") +
   scale_fill_manual(values = pal[c(3,4,5,6,7,8,10)]))
-ggsave(filename="figures/attachment_plots/med_influence_inflow_barplots.png", plot=med_barplot2, height = 6, width = 7, units = "in")
+ggsave(filename="figures/attachment_plots/med_influence_omr_barplots_eis.png", plot=med_barplot_eis, height = 5, width = 6.5, units = "in")
+
+(med_barplot_inflow_eis <- med_barplot_data %>%
+  ggplot() +
+  geom_col(aes(group, pLength, fill = Alt), position= position_dodge(0.75, preserve = "single"), width = 0.6) +
+  geom_text(aes(group, pLength, group = Alt, label = low_n), vjust = -0.1, position= position_dodge(0.75))+
+  facet_wrap(~OMR_Flow, nrow = 4) +
+  labs(y = "Proportional Channel Length", x = "Inflow Group", fill = "Alternative") +
+  theme_bw() +
+    theme(axis.text.x = element_text(angle = 90),
+          legend.position = "top") +
+  scale_fill_manual(values = pal[c(3,4,5,6,7,8,10)]))
+ggsave(filename="figures/attachment_plots/med_influence_inflow_barplots_eis.png", plot=med_barplot_inflow_eis, height =8, width = 6, units = "in")
+
+#** BA figures -----------------------------
+med_barplot_data_ba <- med_barplot_data %>%
+  filter(!(Alt %in% c("Alt1", "Alt4", "Alt2wTUCPwoVA")))
+
+(med_barplot_ba <- med_barplot_data_ba %>%
+   ggplot() +
+   geom_col(aes(OMR_Flow, pLength, fill = Alt), position= position_dodge(0.75, preserve = "single"), width = 0.6) +
+   geom_text(aes(OMR_Flow, pLength, group = Alt, label = low_n), vjust = -0.01, position= position_dodge(0.75))+
+   facet_wrap(~group) +
+   labs(y = "Proportional Channel Length", x = "OMR Bin", fill = "PA Components") +
+   theme_bw() +
+   theme(axis.text.x = element_text(angle = 90),
+         legend.position = "top") +
+    guides(fill = guide_legend(nrow = 2)) +
+   scale_fill_manual(values = pal[c(3,5,6,7)]))
+ggsave(filename="figures/attachment_plots/med_influence_omr_barplots_ba.png", plot=med_barplot_ba, height = 5, width = 6.5, units = "in")
+
+(med_barplot_inflow_ba <- med_barplot_data_ba %>%
+    ggplot() +
+    geom_col(aes(group, pLength, fill = Alt), position= position_dodge(0.75, preserve = "single"), width = 0.6) +
+    geom_text(aes(group, pLength, group = Alt, label = low_n), vjust = -0.1, position= position_dodge(0.75))+
+    facet_wrap(~OMR_Flow, nrow = 4) +
+    labs(y = "Proportional Channel Length", x = "Inflow Group", fill = "PA Components") +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 90),
+          legend.position = "top") +
+    guides(fill = guide_legend(nrow = 2)) +
+    scale_fill_manual(values = pal[c(3,5,6,7)]))
+ggsave(filename="figures/attachment_plots/med_influence_inflow_barplots_ba.png", plot=med_barplot_inflow_ba, height =8, width = 6, units = "in")
+
+
+
+
 
 # look at just high
 filtered2_high %>%
   ggplot() +
-  geom_col(aes(OMR_Flow, pLength, fill = Alt), position= "dodge2") +
+  geom_col(aes(OMR_Flow, pLength, fill = Alt), position= position_dodge(0.75, preserve = "single")) +
   facet_wrap(~group) +
   labs(y = "Proportional Channel Length", title = "High hydro influence") +
   theme_bw() +
@@ -165,18 +217,31 @@ med_table_EIS <- left_join(med_table_long, med_prop) %>%
                 NAA, Alt1, Alt2woTUCPwoVA, Alt2woTUCPDeltaVA, Alt2woTUCPAllVA, everything())
 
 med_table_BA <- med_table %>%
-  dplyr::select(`Inflow group` = group, `OMR bin` = OMR_Flow, NAA, Alt2woTUCPwoVA, Alt2woTUCPDeltaVA, Alt2woTUCPAllVA, Alt2wTUCPwoVA)
+  dplyr::select(`Inflow group` = group, `OMR bin` = OMR_Flow, NAA, Alt2woTUCPwoVA, Alt2woTUCPDeltaVA, Alt2woTUCPAllVA)
 
 # write tables
-# write_csv(med_table_EIS, "data_export/medium_hydro_channel_length_EIS.csv")
-# write_csv(med_table_BA, "data_export/medium_hydro_channel_length_BA.csv")
+# write_csv(med_table_EIS, "data_export/tab8_medium_hydro_channel_length_EIS.csv")
+# write_csv(med_table_BA, "data_export/tab7_medium_hydro_channel_length_BA.csv")
 
-# Calculate lows and highs
-ordered_medium <- med_table_long %>% arrange(sumLength)
+# Calculate lows and highs for text
+ordered_medium <- med_table_long %>% arrange(sumLength) %>%
+  mutate(propLength = round(sumLength/3849200 *100,2))
 
+ordered_medium_prop <- med_prop %>% arrange(changeLength)
+
+ordered_medium_BA <- med_table_long %>% arrange(sumLength) %>%
+  filter(!(Alt %in% c("Alt1", "Alt4", "Alt2wTUCPwoVA", "NAA"))) %>%
+  mutate(propLength = round(sumLength/3849200 *100,2))
+ordered_medium_Alt2 <- med_table_long %>% arrange(sumLength) %>%
+  filter(!(Alt %in% c("Alt1", "Alt4",  "NAA"))) %>%
+  mutate(propLength = round(sumLength/3849200 *100,2))
 
 # write out all the stacked barplots -----------------------------
 lapply(inflow_order, plot_barplot)
+lapply(alt_order, plot_barplot_alt)
+plot_barplot_BA("NAA")
+
+plot_barplot_alt(alt = "NAA")
 
 # individual plot
 plot_barplot("lohi")
