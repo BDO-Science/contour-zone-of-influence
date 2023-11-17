7a2c307a622d799ce6786194c9d2ef7e7c1cdbf6
Cat Pien <catarina.pien@gmail.com>
2023-11-16 16:41:54 -0800

    Changed date of change, added in DSM2 data sample sizes

--- calculate_frequency_bins_alts.R
+++ calculate_frequency_bins_alts.R
@@ -1,5 +1,6 @@
 ##############################################
-# Last updated 10/19/2023 by Catarina Pien (USBR)
+# Last updated 11/16/2023 by Catarina Pien (USBR)
+# Contact: cpien@usbr.gov
 # Calculate sample sizes for each alt in flow and OMR bins
 # There is a version for the BA (Frequency) that needs to be edited
 # and a version for ZOI which also applies to other analyses
@@ -12,21 +13,154 @@ library(readxl)
 library(lubridate)
 library(viridis)
 library(tidyr)
+library(here)
+library(purrr)
 
-# This part applies to both sections.
-# Read data -------------------------------
+# DSM2 data input ---------------------------
+# This version of frequency is currently the most up to date.
+## Read data -------------------------------
 
 # order
 alt_order = c("EXP1", "EXP3", "NAA","ALT1",
               "ALT2d", "ALT2b", "ALT2c", "ALT2a", "ALT3", "ALT4")
+alt_order2 =c("NAA","Alt1","Alt2woTUCPwoVA","Alt2woTUCPDeltaVA", "Alt2woTUCPAllVA", "Alt2wTUCPwoVA", "Alt4")
 col_order = c("Flow", "OMR", "OMR_range",  "EXP1", "EXP3", "NAA",
               "ALT1",
               "ALT2d", "ALT2b", "ALT2c", "ALT2a",
               "ALT3", "ALT4")
 inflow_order = c("lolo", "lomed", "lohi", "medlo", "medmed", "medhi", "hilo", "himed", "hihi", "NA")
-omr_order = c("< -3500", ">= -3500")
+omr_order = c("-2000","-3500","-5000", "<-5500",  "NA")
 
-# read in data
+
+
+# Read in data (DSM2 processing)
+
+fp <- dir(here::here("data_raw/DSM2_samples"), full.names = TRUE)
+dsm2bins <- map(fp, read_csv, col_types = cols(.default = "c"))
+walk(dsm2bins, glimpse)
+
+bins <- bind_rows(dsm2bins, .id = "id") %>%
+  mutate(Alt = case_when(id == 1 ~ "Alt1",
+                         id ==2 ~ "Alt2b",
+                         id == 3 ~ "Alt2a",
+                         id == 4 ~ "Alt2c",
+                         id ==5 ~ "Alt2d",
+                         id == 6 ~ "Alt4",
+                         id == 7~ "NAA")) %>%
+  mutate(Sub.group = replace(Sub.group, is.na(Sub.group), "NA")) %>%
+  rename(Date = month_year) %>%
+  dplyr::select(-Date_x) %>%
+  distinct() %>%
+  mutate(OMR = case_when(OMR_bin == -13000 ~ "<-5500",
+                         OMR_bin == 99999 ~ "NA",
+                         TRUE~ OMR_bin)) %>%
+  mutate(Alt = case_when(Alt == "Alt2b" ~ "Alt2woTUCPwoVA",
+                         Alt == "Alt2c" ~ "Alt2woTUCPDeltaVA",
+                         Alt == "Alt2d" ~ "Alt2woTUCPAllVA",
+                         Alt == "Alt2a" ~ "Alt2wTUCPwoVA",
+                         TRUE ~ Alt)) %>%
+  mutate(OMR = factor(OMR, levels = omr_order),
+         Alt = factor(Alt, levels = alt_order2),
+         Sub.group = factor(Sub.group, levels = inflow_order))
+
+
+## calculate sample sizes --------------------------
+# this is used later for channel length code
+
+# OMR and inflow bins
+n_flow_OMR_Alt <- bins %>%
+  group_by(Alt, Sub.group, OMR) %>%
+  summarize(n = n(),
+            percent = round(n/700*100))%>%
+  ungroup() %>%
+  complete(Sub.group, OMR, nesting(Alt),
+           fill = list(n = 0))  %>%
+  ungroup()
+
+
+# inflow bins only
+n_flow_Alt <- bins %>%
+  group_by(Alt, Sub.group) %>%
+  summarize(n = n(),
+            percent = round(n/700*100))%>%
+  ungroup() %>%
+  complete(Sub.group, nesting(Alt),
+           fill = list(n = 0))
+
+### write out ------------------------
+# write_csv(n_flow_OMR_Alt, "data_export/samplesizes_flow_OMR_alt.csv")
+# write_csv(n_flow_Alt, "data_export/samplesizes_flow_alt.csv")
+
+### plot sample size data --------------------------------
+pal <- c('#9a3324', "#88CCEE","#AA4499",'#003E51','#007396', '#C69214', '#DDCBA4','#FF671F', '#215732','#4C12A1')
+
+(dsm_sample_plot_flow_omr <- ggplot(n_flow_OMR_Alt) + geom_tile(aes(x = Alt, y = OMR, fill = n), color = "gray70") +
+  geom_text(aes(x = Alt, y = OMR, label = n), color = "white") +
+  facet_wrap(~Sub.group) +
+  scale_fill_viridis(option = "plasma") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90)))
+ggsave("figures/plot_dsm2_samples_omr_inflow.png",
+       plot = dsm_sample_plot_flow_omr, units = "in", width = 8, height = 8)
+
+(dsm_sample_plot_flow_omr <- ggplot(n_flow_OMR_Alt) + geom_tile(aes(x = Alt, y = OMR, fill = n), color = "gray70") +
+  geom_text(aes(x = Alt, y = OMR, label = n), color = "white") +
+  facet_wrap(~Sub.group) +
+    labs(y = "OMR bin (cfs)") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90),
+        legend.position = "top",
+        axis.title.x = element_blank(),
+        strip.text = element_text(size= 10)))
+ggsave("figures/plot_dsm2_samples_omr_inflow_blue.png",
+       plot = dsm_sample_plot_flow_omr, units = "in", width = 8, height = 9)
+
+(dsm_sample_barplot_flow <- ggplot(data = n_flow_OMR_Alt, aes(x = Alt, y = n, fill = OMR), position = dodge2) +
+    geom_col() +
+    facet_wrap(~Sub.group) +
+    geom_text(aes(label = n), position = position_stack(vjust = .5), color = "gray50") +
+    scale_fill_viridis_d(option = "plasma") +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 90)))
+ggsave("figures/plot_dsm2_samples_inflow.png",
+       plot = dsm_sample_plot_flow, units = "in", width = 8, height = 8)
+
+(dsm_sample_plot_flow <- ggplot(data = n_flow_Alt, aes(x = Alt, y = percent, fill = Sub.group)) +
+  geom_col() +
+  geom_text(aes(label = percent), position = position_stack(vjust = .5), color = "gray50") +
+  scale_fill_viridis_d(option = "plasma") +
+  theme(axis.text.x = element_text(angle = 90)))
+ggsave("figures/plot_dsm2_samples_inflow.png",
+       plot = dsm_sample_plot_flow, units = "in", width = 8, height = 8)
+
+### summary stats tables ---------------------------------
+dsm2_samples_inflow_table <- n_flow_Alt %>%
+  rename(`Inflow Group` = Sub.group) %>%
+  select(-percent) %>%
+  pivot_wider(names_from = "Alt", values_from = "n", values_fill = 0)
+
+dsm2_samples_inflow_omr_table <- n_flow_OMR_Alt %>%
+  rename(`Inflow Group` = Sub.group, `OMR Bin` = "OMR") %>%
+  select(-percent) %>%
+  pivot_wider(names_from = "Alt", values_from = "n", values_fill = 0) %>%
+  arrange(`Inflow Group`, `OMR Bin`)
+write_csv(dsm2_samples_inflow_omr_table, "data_export/tab4_samplesizes_inflowgroups_omr_alts_dsm2.csv")
+
+samples_excluded <- n_flow_OMR_Alt %>%
+  select(-percent, -Sub.group) %>%
+  filter(OMR == "NA") %>%
+  group_by(Alt, OMR) %>%
+  summarize(prop = sum(n)/700)%>%
+  pivot_wider(names_from = "Alt",
+              values_from = prop)
+write_csv(dsm2_samples_inflow_table, "data_export/tab2_samplesizes_inflowgroups_alts_dsm2.csv")
+
+
+# Calsim 3 input data-----------------------------
+# These data are used for other analyses and were used to determine bins. However, these do not
+# reflect actual sample sizes for zoi analysis (similar but slight differences)
+
+# read in data (Calsim inputs from Steve's binning document) -----------------------
 bins <- read_excel("data_raw/calsim/Reclamation_2021LTO_SacR_SJR_OMR_Binning_rev01_20230929_result.xlsx", skip = 5)
 # rename column names
 colnames(bins) <- c("Date", "Flow_EXP1", "OMR_EXP1", "Flow_EXP3", "OMR_EXP3",
@@ -37,7 +171,7 @@ colnames(bins) <- c("Date", "Flow_EXP1", "OMR_EXP1", "Flow_EXP3", "OMR_EXP3",
 bins2 <- bins[-1,]
 
 # Start here for ZOI analysis. Skip to BA section for BA analysis.
-# ZOI -------------------------------
+## ZOI -------------------------------
 ## reformat ----------------------
 ###  make long ---------
 
@@ -220,15 +354,6 @@ bins_freq <- bins_months %>%
   rename(OMR_val = OMR,
          OMR = OMR_group)
 
-# bins_freq <- bins_months %>%
-#   mutate(OMR_group = case_when(OMR > -1500 ~ "positive",
-#                                 OMR >= -2500 & OMR <=-1500 ~ "-2000",
-#                                 OMR >= -4000 & OMR <=-3000 ~ "-3500",
-#                                 OMR >= -5500 & OMR <=-4500 ~ "-5000",
-#                                 OMR <=-5500 ~ "less than -5500",
-#                                 TRUE ~ "Other")) %>%
-#   rename(OMR_val = OMR,
-#          OMR = OMR_group)
 
 ### calculate sample sizes -----------------
 bins_summary_months <- bins_freq %>%
